4a16bffc25abb6b32e8ef1d7f4721e79ba763ae5
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 23:36:31 +0100

    to_do_application

--- src/test/java/africa/semicolon/contactManagementService/servicesTest/ContactServiceTest.java
+++ src/test/java/africa/semicolon/contactManagementService/servicesTest/ContactServiceTest.java
@@ -1,20 +1,32 @@
 package africa.semicolon.contactManagementService.servicesTest;
 
 import africa.semicolon.contactManagementService.datas.models.Contact;
+import africa.semicolon.contactManagementService.datas.repositories.ContactRepository;
+import africa.semicolon.contactManagementService.dtos.ContactCreationRequest;
 import africa.semicolon.contactManagementService.services.contactService.ContactService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
 public class ContactServiceTest {
 
     @Autowired
     private ContactService contactService;
+    @Autowired
+    private ContactRepository contactRepository;
 
     @Test
     public void createContact_contactIsCreatedTest() {
-        Contact contact = new Contact();
-
+        ContactCreationRequest contactCreationRequest = new ContactCreationRequest();
+        contactCreationRequest.setFirstName("John");
+        contactCreationRequest.setLastName("Doe");
+        contactCreationRequest.setEmail("john@doe.com");
+        contactCreationRequest.setPhoneNumber("1234567890");
+        contactService.createContact(contactCreationRequest);
+        assertThat(contactRepository.count(), is(1L));
     }
 }

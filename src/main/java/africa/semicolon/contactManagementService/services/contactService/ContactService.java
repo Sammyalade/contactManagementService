160b7112ba6b6ad33ae5ea14b3c7edb858f0119f
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 23:23:03 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/contactService/ContactService.java
+++ src/main/java/africa/semicolon/contactManagementService/services/contactService/ContactService.java
@@ -1,4 +1,9 @@
 package africa.semicolon.contactManagementService.services.contactService;
 
+import africa.semicolon.contactManagementService.datas.models.Contact;
+import africa.semicolon.contactManagementService.dtos.ContactCreationRequest;
+
 public interface ContactService {
+
+    Contact createContact(ContactCreationRequest contactCreationRequest);
 }

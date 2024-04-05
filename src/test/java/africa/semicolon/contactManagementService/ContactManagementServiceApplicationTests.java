8e948a9a9cc7528b4f6129bce9de76a0bae65012
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 12:01:45 +0100

    to_do_application

---
+++ src/test/java/africa/semicolon/contactManagementService/ContactManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.semicolon.contactManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

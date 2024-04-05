a1ed62a23165a21ea57559c05cc9b61755cf5dba
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 23:23:41 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/utility/Mapper.java
+++ src/main/java/africa/semicolon/contactManagementService/utility/Mapper.java
@@ -3,6 +3,9 @@ package africa.semicolon.contactManagementService.utility;
 import africa.semicolon.contactManagementService.datas.models.User;
 import africa.semicolon.contactManagementService.dtos.UserCreationRequest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static africa.semicolon.contactManagementService.utility.Utility.toLowerCase;
 
 public class Mapper {
@@ -17,5 +20,11 @@ public class Mapper {
 
     public static boolean checkEmptyStringUsername(String username) {
         return username.isEmpty();
+    }
+
+    public static List<?> checkIfListIsNull(List<?> lists) {
+        if(lists == null) lists = new ArrayList<>();
+
+        return lists;
     }
 }

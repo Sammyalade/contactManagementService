2742a4f33c85fd8460228f559d5ce6c07fca68f1
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 23:23:28 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/contactService/ContactServiceImpl.java
+++ src/main/java/africa/semicolon/contactManagementService/services/contactService/ContactServiceImpl.java
@@ -1,7 +1,13 @@
 package africa.semicolon.contactManagementService.services.contactService;
 
+import africa.semicolon.contactManagementService.datas.models.Contact;
+import africa.semicolon.contactManagementService.dtos.ContactCreationRequest;
 import org.springframework.stereotype.Service;
 
 @Service("contactServiceImpl")
 public class ContactServiceImpl implements ContactService{
+    @Override
+    public Contact createContact(ContactCreationRequest contactCreationRequest) {
+        return null;
+    }
 }

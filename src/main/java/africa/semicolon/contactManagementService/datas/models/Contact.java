d43f87ae5b2e14cb64b48723069a56d730a85210
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 13:11:47 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/datas/models/Contact.java
+++ src/main/java/africa/semicolon/contactManagementService/datas/models/Contact.java
@@ -1,4 +1,20 @@
 package africa.semicolon.contactManagementService.datas.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@Entity
+@Table(name = "contact_table")
 public class Contact {
+
+    @Id
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNumber;
 }

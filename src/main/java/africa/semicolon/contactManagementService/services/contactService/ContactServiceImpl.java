5247ec532cb12620b399e4fff61e1ef8259c645f
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 22:47:55 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/contactService/ContactServiceImpl.java
+++ src/main/java/africa/semicolon/contactManagementService/services/contactService/ContactServiceImpl.java
@@ -1,4 +1,7 @@
 package africa.semicolon.contactManagementService.services.contactService;
 
+import org.springframework.stereotype.Service;
+
+@Service("contactServiceImpl")
 public class ContactServiceImpl implements ContactService{
 }

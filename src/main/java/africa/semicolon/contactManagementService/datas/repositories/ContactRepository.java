82347313e6c3614ad643a327726b3c1a2f10b7f4
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 12:26:36 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/datas/repositories/ContactRepository.java
+++ src/main/java/africa/semicolon/contactManagementService/datas/repositories/ContactRepository.java
@@ -1,4 +1,7 @@
 package africa.semicolon.contactManagementService.datas.repositories;
 
-public class ContactRepository {
+import africa.semicolon.contactManagementService.datas.models.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
 }

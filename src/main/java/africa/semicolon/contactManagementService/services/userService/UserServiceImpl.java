00dd3f595ba9cb41eda6bfc274a2d651d238adb1
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 23:03:17 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/userService/UserServiceImpl.java
+++ src/main/java/africa/semicolon/contactManagementService/services/userService/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package africa.semicolon.contactManagementService.services.userService;
 
 
+import africa.semicolon.contactManagementService.datas.models.User;
 import africa.semicolon.contactManagementService.datas.repositories.UserRepository;
 import africa.semicolon.contactManagementService.dtos.UserCreationRequest;
+import africa.semicolon.contactManagementService.exception.EmptyStringException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static africa.semicolon.contactManagementService.utility.Mapper.checkEmptyStringUsername;
 import static africa.semicolon.contactManagementService.utility.Mapper.map;
 
 
@@ -15,8 +18,14 @@ public class UserServiceImpl implements UserService{
     @Autowired
     private UserRepository userRepository;
 
-    public void createUser(UserCreationRequest userCreationRequest){
-        userRepository.save(map(userCreationRequest ));
+    public User createUser(UserCreationRequest userCreationRequest){
+        if(checkEmptyStringUsername(userCreationRequest.getUsername()))
+            throw new EmptyStringException("Username cannot be null");
+        User newUser = map(userCreationRequest);
+        userRepository.save(newUser);
+        return newUser;
     }
+
+
 
 }

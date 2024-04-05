14cbeec94344fa630204886f122c5f9b8513f039
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 23:28:18 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/contactService/ContactServiceImpl.java
+++ src/main/java/africa/semicolon/contactManagementService/services/contactService/ContactServiceImpl.java
@@ -1,13 +1,22 @@
 package africa.semicolon.contactManagementService.services.contactService;
 
 import africa.semicolon.contactManagementService.datas.models.Contact;
+import africa.semicolon.contactManagementService.datas.repositories.ContactRepository;
 import africa.semicolon.contactManagementService.dtos.ContactCreationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static africa.semicolon.contactManagementService.utility.Mapper.map;
 
 @Service("contactServiceImpl")
 public class ContactServiceImpl implements ContactService{
+
+    @Autowired
+    private ContactRepository contactRepository;
     @Override
     public Contact createContact(ContactCreationRequest contactCreationRequest) {
-        return null;
+        Contact contact = map(contactCreationRequest);
+        contactRepository.save(contact);
+        return contact;
     }
 }

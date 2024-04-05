8660ee0c4954f9dc4cc1c5090a0c2d6793651da3
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 13:12:11 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/datas/models/Group.java
+++ src/main/java/africa/semicolon/contactManagementService/datas/models/Group.java
@@ -1,4 +1,20 @@
 package africa.semicolon.contactManagementService.datas.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Data
+@RequiredArgsConstructor
+@Entity
+@Table(name = "group_table")
 public class Group {
+
+    @Id
+    private int id;
+    private String name;
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+    private List<Contact> contacts;
 }

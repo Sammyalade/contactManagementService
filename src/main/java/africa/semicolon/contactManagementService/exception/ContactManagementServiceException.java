f3bfd638a0a4461ae571ade8712654dcee056676
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 23:01:21 +0100

    to_do_application

---
+++ src/main/java/africa/semicolon/contactManagementService/exception/ContactManagementServiceException.java
@@ -0,0 +1,8 @@
+package africa.semicolon.contactManagementService.exception;
+
+public class ContactManagementServiceException  extends RuntimeException{
+
+    public ContactManagementServiceException(String message) {
+        super(message);
+    }
+}

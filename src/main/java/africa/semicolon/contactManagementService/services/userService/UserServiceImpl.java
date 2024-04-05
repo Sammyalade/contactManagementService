1739f696bdc561b7f6b44dedee2bf9b8df9d3970
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 22:48:31 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/userService/UserServiceImpl.java
+++ src/main/java/africa/semicolon/contactManagementService/services/userService/UserServiceImpl.java
@@ -1,4 +1,22 @@
 package africa.semicolon.contactManagementService.services.userService;
 
+
+import africa.semicolon.contactManagementService.datas.repositories.UserRepository;
+import africa.semicolon.contactManagementService.dtos.UserCreationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static africa.semicolon.contactManagementService.utility.Mapper.map;
+
+
+@Service
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void createUser(UserCreationRequest userCreationRequest){
+        userRepository.save(map(userCreationRequest ));
+    }
+
 }

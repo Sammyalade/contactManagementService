9aa373c201748b04f4a09468e6b9a21bcbd9bfd4
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 22:49:30 +0100

    to_do_application

---
+++ src/main/java/africa/semicolon/contactManagementService/utility/Utility.java
@@ -0,0 +1,12 @@
+package africa.semicolon.contactManagementService.utility;
+
+import africa.semicolon.contactManagementService.datas.models.Contact;
+
+import java.util.ArrayList;
+
+public class Utility {
+
+    public static String toLowerCase(String input) {
+        return input.toLowerCase();
+    }
+}

df850b1239275689484e661f90971324d212716c
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 23:02:29 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/userService/UserService.java
+++ src/main/java/africa/semicolon/contactManagementService/services/userService/UserService.java
@@ -1,8 +1,9 @@
 package africa.semicolon.contactManagementService.services.userService;
 
+import africa.semicolon.contactManagementService.datas.models.User;
 import africa.semicolon.contactManagementService.dtos.UserCreationRequest;
 
 public interface UserService {
 
-    void createUser(UserCreationRequest userCreationRequest);
+    User createUser(UserCreationRequest userCreationRequest);
 }

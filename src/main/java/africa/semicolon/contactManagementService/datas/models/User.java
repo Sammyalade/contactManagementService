f2eea9ba1a04d577d73fcf916e796a429f7fb6cb
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 13:12:29 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/datas/models/User.java
+++ src/main/java/africa/semicolon/contactManagementService/datas/models/User.java
@@ -1,4 +1,24 @@
 package africa.semicolon.contactManagementService.datas.models;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
+@Data
+@RequiredArgsConstructor
+@Entity
+@Table(name = "user_table")
 public class User {
+
+    @Id
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Contact> contacts;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Group> groups;
+
 }

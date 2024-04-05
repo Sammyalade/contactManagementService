d7b6693ba24a4784a34d3a5811ef00160a985bac
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-05 12:26:53 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/datas/repositories/UserRepository.java
+++ src/main/java/africa/semicolon/contactManagementService/datas/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package africa.semicolon.contactManagementService.datas.repositories;
 
-public class UserRepository {
+import africa.semicolon.contactManagementService.datas.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }

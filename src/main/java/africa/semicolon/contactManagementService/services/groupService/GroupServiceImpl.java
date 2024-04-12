aa173fd4e6bfe2694d2c868dcfa8d4e8ee2e4e91
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-12 01:39:47 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/groupService/GroupServiceImpl.java
+++ src/main/java/africa/semicolon/contactManagementService/services/groupService/GroupServiceImpl.java
@@ -1,4 +1,54 @@
 package africa.semicolon.contactManagementService.services.groupService;
 
+import africa.semicolon.contactManagementService.datas.models.Contact;
+import africa.semicolon.contactManagementService.datas.models.Group;
+import africa.semicolon.contactManagementService.dtos.*;
+
+import java.util.List;
+
 public class GroupServiceImpl implements GroupService{
+    @Override
+    public Group createGroup(CreateGroupRequest request) {
+        return null;
+    }
+
+    @Override
+    public Group getGroupById(Long groupId) {
+        return null;
+    }
+
+    @Override
+    public Group updateGroup(UpdateGroupRequest request) {
+        return null;
+    }
+
+    @Override
+    public void deleteGroup(Long groupId) {
+
+    }
+
+    @Override
+    public List<Group> listGroups(ListGroupsRequest request) {
+        return null;
+    }
+
+    @Override
+    public void addContactToGroup(AddContactToGroupRequest request) {
+
+    }
+
+    @Override
+    public void removeContactFromGroup(RemoveContactFromGroupRequest request) {
+
+    }
+
+    @Override
+    public List<Contact> getContactsInGroup(Long groupId) {
+        return null;
+    }
+
+    @Override
+    public List<Group> searchGroups(SearchGroupsRequest request) {
+        return null;
+    }
 }

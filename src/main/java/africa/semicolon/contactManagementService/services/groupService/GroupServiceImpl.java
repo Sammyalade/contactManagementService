11e981ff8d4df792502c3c3848bd06d86a79c2ca
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-12 23:06:29 +0100

    contact_management_service

--- src/main/java/africa/semicolon/contactManagementService/services/groupService/GroupServiceImpl.java
+++ src/main/java/africa/semicolon/contactManagementService/services/groupService/GroupServiceImpl.java
@@ -2,34 +2,51 @@ package africa.semicolon.contactManagementService.services.groupService;
 
 import africa.semicolon.contactManagementService.datas.models.Contact;
 import africa.semicolon.contactManagementService.datas.models.Group;
+import africa.semicolon.contactManagementService.datas.repositories.GroupRepository;
 import africa.semicolon.contactManagementService.dtos.*;
+import africa.semicolon.contactManagementService.exception.EmptyStringException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service("groupService")
 public class GroupServiceImpl implements GroupService{
+
+    @Autowired
+    private GroupRepository groupRepository;
     @Override
-    public Group createGroup(CreateGroupRequest request) {
-        return null;
+    public Group createGroup(String name) {
+        if(name.isEmpty()) throw new EmptyStringException("Group name cannot be empty");
+        Group group = new Group();
+        group.setName(name);
+        groupRepository.save(group);
+        return group;
     }
 
     @Override
-    public Group getGroupById(Long groupId) {
-        return null;
+    public Group getGroupById(int groupId) {
+        Optional<Group> group = groupRepository.findById(groupId);
+        return group.orElse(null);
     }
 
     @Override
     public Group updateGroup(UpdateGroupRequest request) {
-        return null;
+        Group group = getGroupById(request.getGroupId());
+        group.setName(request.getGroupName());
+        groupRepository.save(group);
+        return group;
     }
 
     @Override
-    public void deleteGroup(Long groupId) {
-
+    public void deleteGroup(int groupId) {
+        groupRepository.deleteById(groupId);
     }
 
     @Override
-    public List<Group> listGroups(ListGroupsRequest request) {
-        return null;
+    public List<Group> findAll() {
+        return groupRepository.findAll();
     }
 
     @Override

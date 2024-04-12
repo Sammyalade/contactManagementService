8eab14ac8ac92691a0742017d9ea775c1cf42780
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-12 01:33:41 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/datas/models/Group.java
+++ src/main/java/africa/semicolon/contactManagementService/datas/models/Group.java
@@ -3,6 +3,7 @@ package africa.semicolon.contactManagementService.datas.models;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
 public class Group {
 
     @Id
+    @GeneratedValue
     private int id;
     private String name;
     @OneToMany(cascade = CascadeType.ALL)

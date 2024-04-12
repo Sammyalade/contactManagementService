443c35f6da1239d6c94a9ae9aed69f9369c60e18
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-12 01:39:04 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/contactService/ContactService.java
+++ src/main/java/africa/semicolon/contactManagementService/services/contactService/ContactService.java
@@ -1,9 +1,23 @@
 package africa.semicolon.contactManagementService.services.contactService;
 
 import africa.semicolon.contactManagementService.datas.models.Contact;
-import africa.semicolon.contactManagementService.dtos.ContactCreationRequest;
+import africa.semicolon.contactManagementService.dtos.*;
+
+import java.util.List;
 
 public interface ContactService {
 
     Contact createContact(ContactCreationRequest contactCreationRequest);
+    Contact getContactById(int contactId);
+
+    Contact updateContact(ContactUpdateRequest request);
+
+    void deleteContact(int contactId);
+
+    List<Contact> findAll();
+
+    void assignContactToGroup(AssignContactToGroupRequest request);
+
+    void removeContactFromGroup(RemoveContactFromGroupRequest request);
+
 }

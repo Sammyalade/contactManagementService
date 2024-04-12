3948e4430e7f05520f6ba3866858783c421a09b0
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-12 23:05:52 +0100

    contact_management_service

--- src/main/java/africa/semicolon/contactManagementService/dtos/UpdateGroupRequest.java
+++ src/main/java/africa/semicolon/contactManagementService/dtos/UpdateGroupRequest.java
@@ -1,4 +1,10 @@
 package africa.semicolon.contactManagementService.dtos;
 
+import lombok.Data;
+
+@Data
 public class UpdateGroupRequest {
+
+    private int groupId;
+    private String groupName;
 }

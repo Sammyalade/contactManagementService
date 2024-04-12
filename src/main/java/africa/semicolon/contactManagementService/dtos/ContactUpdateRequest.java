5dcb95dc639e49b44b6a65107a9a608e47450dba
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-12 01:36:19 +0100

    to_do_application

---
+++ src/main/java/africa/semicolon/contactManagementService/dtos/ContactUpdateRequest.java
@@ -0,0 +1,15 @@
+package africa.semicolon.contactManagementService.dtos;
+
+import africa.semicolon.contactManagementService.datas.models.User;
+import lombok.Data;
+
+@Data
+public class ContactUpdateRequest {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private User user;
+}

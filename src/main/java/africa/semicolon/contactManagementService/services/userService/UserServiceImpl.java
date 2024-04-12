5a5427e9a483f1a6f6605232c5fe62b55015aaa6
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-12 01:40:30 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/userService/UserServiceImpl.java
+++ src/main/java/africa/semicolon/contactManagementService/services/userService/UserServiceImpl.java
@@ -4,8 +4,7 @@ package africa.semicolon.contactManagementService.services.userService;
 import africa.semicolon.contactManagementService.datas.models.Contact;
 import africa.semicolon.contactManagementService.datas.models.User;
 import africa.semicolon.contactManagementService.datas.repositories.UserRepository;
-import africa.semicolon.contactManagementService.dtos.ContactCreationRequest;
-import africa.semicolon.contactManagementService.dtos.UserCreationRequest;
+import africa.semicolon.contactManagementService.dtos.*;
 import africa.semicolon.contactManagementService.exception.EmptyStringException;
 import africa.semicolon.contactManagementService.services.contactService.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,12 +31,48 @@ public class UserServiceImpl implements UserService{
         return newUser;
     }
 
+
     @Override
     public void createContact(ContactCreationRequest contactCreationRequest) {
         Contact contact = contactService.createContact(contactCreationRequest);
         User user = contactCreationRequest.getUser();
         user.setContacts((List<Contact>) checkIfListIsNull(user.getContacts()));
         user.getContacts().add(contact);
+    }
+
+    @Override
+    public void loginUser(UserLoginRequest userLoginRequest) {
+
+    }
+
+    @Override
+    public void logoutUser(String username) {
+
+    }
+
+    @Override
+    public void updateUser(UserUpdateRequest userUpdateRequest) {
+
+    }
+
+    @Override
+    public void deleteUser(UserDeleteRequest userDeleteRequest) {
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void changePassword(UserPasswordChangeRequest userPasswordChangeRequest) {
+
+    }
+
+    @Override
+    public void resetPassword(UserPasswordResetRequest userPasswordResetRequest) {
+
     }
 
 

e2cc706db9287555124f1f49aeb035bc128d2e7f
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-12 01:40:01 +0100

    to_do_application

--- src/main/java/africa/semicolon/contactManagementService/services/userService/UserService.java
+++ src/main/java/africa/semicolon/contactManagementService/services/userService/UserService.java
@@ -1,12 +1,21 @@
 package africa.semicolon.contactManagementService.services.userService;
 
 import africa.semicolon.contactManagementService.datas.models.User;
-import africa.semicolon.contactManagementService.dtos.ContactCreationRequest;
-import africa.semicolon.contactManagementService.dtos.UserCreationRequest;
+import africa.semicolon.contactManagementService.dtos.*;
+
+import java.util.List;
 
 public interface UserService {
 
     User createUser(UserCreationRequest userCreationRequest);
 
     void createContact(ContactCreationRequest contactCreationRequest);
+    void loginUser(UserLoginRequest userLoginRequest);
+    void logoutUser(String username);
+    void updateUser(UserUpdateRequest userUpdateRequest);
+    void deleteUser(UserDeleteRequest userDeleteRequest);
+    List<User> getAllUsers();
+    void changePassword(UserPasswordChangeRequest userPasswordChangeRequest);
+    void resetPassword(UserPasswordResetRequest userPasswordResetRequest);
+
 }

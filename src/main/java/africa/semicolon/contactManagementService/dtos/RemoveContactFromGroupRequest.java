1611cbb661faab9def4e5f8315d53ff054b7843d
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-13 07:05:56 +0100

    contact_management_service

--- src/main/java/africa/semicolon/contactManagementService/dtos/RemoveContactFromGroupRequest.java
+++ src/main/java/africa/semicolon/contactManagementService/dtos/RemoveContactFromGroupRequest.java
@@ -1,4 +1,10 @@
 package africa.semicolon.contactManagementService.dtos;
 
+import lombok.Data;
+
+@Data
 public class RemoveContactFromGroupRequest {
+
+    private int contactId;
+    private int groupId;
 }

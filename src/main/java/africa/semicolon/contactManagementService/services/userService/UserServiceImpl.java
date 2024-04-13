eb2b991326fa69f8ae815f1cac9fa3ae6f3c1754
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-13 22:10:18 +0100

    contact_management_service

--- src/main/java/africa/semicolon/contactManagementService/services/userService/UserServiceImpl.java
+++ src/main/java/africa/semicolon/contactManagementService/services/userService/UserServiceImpl.java
@@ -4,13 +4,21 @@ package africa.semicolon.contactManagementService.services.userService;
 import africa.semicolon.contactManagementService.datas.models.Contact;
 import africa.semicolon.contactManagementService.datas.models.User;
 import africa.semicolon.contactManagementService.datas.repositories.UserRepository;
-import africa.semicolon.contactManagementService.dtos.*;
+import africa.semicolon.contactManagementService.dtos.requests.ContactCreationRequest;
+import africa.semicolon.contactManagementService.dtos.requests.UserCreationRequest;
+import africa.semicolon.contactManagementService.dtos.requests.UserLoginRequest;
+import africa.semicolon.contactManagementService.dtos.requests.UserUpdateRequest;
+import africa.semicolon.contactManagementService.dtos.responses.ContactCreationResponse;
+import africa.semicolon.contactManagementService.dtos.responses.UserRegistrationResponse;
+import africa.semicolon.contactManagementService.exception.EmailAlreadyRegisteredException;
 import africa.semicolon.contactManagementService.exception.EmptyStringException;
+import africa.semicolon.contactManagementService.exception.UserNotFoundException;
 import africa.semicolon.contactManagementService.services.contactService.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import static africa.semicolon.contactManagementService.utility.Mapper.*;
 
@@ -23,57 +31,69 @@ public class UserServiceImpl implements UserService{
     @Autowired
     private ContactService contactService;
 
-    public User createUser(UserCreationRequest userCreationRequest){
-        if(checkEmptyStringUsername(userCreationRequest.getUsername()))
-            throw new EmptyStringException("Username cannot be null");
-        User newUser = map(userCreationRequest);
-        userRepository.save(newUser);
-        return newUser;
+    @Override
+    public UserRegistrationResponse registerUser(UserCreationRequest userCreationRequest) {
+        if (isEmptyOrNullString(userCreationRequest.getUsername())) throw new EmptyStringException("Username cannot be null or empty");
+        if(userRepository.findByEmail(userCreationRequest.getEmail()) != null)
+            throw new EmailAlreadyRegisteredException("Email already registered. Please login instead");
+        User user = map(userCreationRequest);
+        userRepository.save(user);
+        return map(user);
+    }
+
+    @Override
+    public ContactCreationResponse createContact(ContactCreationRequest contactCreationRequest) {
+        User user = searchUserById(contactCreationRequest.getUserId());
+        Contact contact = contactService.createContact(contactCreationRequest);
+        user.setContacts((List<Contact>) checkIfListIsNull(user.getContacts()));
+        user.getContacts().add(contact);
+        userRepository.save(user);
+        return map(user, contact);
     }
 
 
     @Override
-    public void createContact(ContactCreationRequest contactCreationRequest) {
-        Contact contact = contactService.createContact(contactCreationRequest);
-        User user = contactCreationRequest.getUser();
-        user.setContacts((List<Contact>) checkIfListIsNull(user.getContacts()));
-        user.getContacts().add(contact);
+    public User searchUserById(int userId) {
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent()){
+            return user.get();
+        }
+        throw new UserNotFoundException("User not found");
     }
+
 
     @Override
     public void loginUser(UserLoginRequest userLoginRequest) {
-
+        User user = searchUserById(userLoginRequest.getUserId());
+        if(user.getEmail().equals(userLoginRequest.getEmail())) user.setLocked(false);
     }
 
     @Override
-    public void logoutUser(String username) {
-
+    public void logoutUser(int id) {
+        User user = searchUserById(id);
+        user.setLocked(true);
     }
 
     @Override
-    public void updateUser(UserUpdateRequest userUpdateRequest) {
-
+    public UserUpdateResponse updateUser(UserUpdateRequest userUpdateRequest) {
+        User user = searchUserById(userUpdateRequest.getId());
+        if(!user.isLocked()) {
+            if(userUpdateRequest.getUsername() != null) user.setUsername(userUpdateRequest.getUsername());
+            if(userUpdateRequest.getEmail() != null) user.setEmail(userUpdateRequest.getEmail());
+            userRepository.save(user);
+            return map(userUpdateRequest);
+        }
+        throw new UserNotLoggedInException("User not logged in. Please login and try again");
     }
+
+
 
     @Override
-    public void deleteUser(UserDeleteRequest userDeleteRequest) {
-
+    public void deleteUser(int id) {
+        User user = searchUserById(id);
+        if(!user.isLocked()) {
+            userRepository.delete(user);
+        } else throw new UserNotLoggedInException("User not logged in. Please login and try again");
     }
-
-    @Override
-    public List<User> getAllUsers() {
-        return null;
-    }
-
-    @Override
-    public void changePassword(UserPasswordChangeRequest userPasswordChangeRequest) {
-
-    }
-
-    @Override
-    public void resetPassword(UserPasswordResetRequest userPasswordResetRequest) {
-
-    }
-
 
 }

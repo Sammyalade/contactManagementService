e1b2aa9827be9f6b518e7304e75e8c42f12ccf3b
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-13 22:09:57 +0100

    contact_management_service

--- src/main/java/africa/semicolon/contactManagementService/services/userService/UserService.java
+++ src/main/java/africa/semicolon/contactManagementService/services/userService/UserService.java
@@ -2,20 +2,22 @@ package africa.semicolon.contactManagementService.services.userService;
 
 import africa.semicolon.contactManagementService.datas.models.User;
 import africa.semicolon.contactManagementService.dtos.*;
+import africa.semicolon.contactManagementService.dtos.requests.ContactCreationRequest;
+import africa.semicolon.contactManagementService.dtos.requests.UserCreationRequest;
+import africa.semicolon.contactManagementService.dtos.requests.UserLoginRequest;
+import africa.semicolon.contactManagementService.dtos.responses.ContactCreationResponse;
+import africa.semicolon.contactManagementService.dtos.responses.UserRegistrationResponse;
 
 import java.util.List;
 
 public interface UserService {
 
-    User createUser(UserCreationRequest userCreationRequest);
 
-    void createContact(ContactCreationRequest contactCreationRequest);
+    UserRegistrationResponse registerUser(UserCreationRequest userCreationRequest);
+
+    ContactCreationResponse createContact(ContactCreationRequest contactCreationRequest);
+
+    User searchUserById(int userId);
+
     void loginUser(UserLoginRequest userLoginRequest);
-    void logoutUser(String username);
-    void updateUser(UserUpdateRequest userUpdateRequest);
-    void deleteUser(UserDeleteRequest userDeleteRequest);
-    List<User> getAllUsers();
-    void changePassword(UserPasswordChangeRequest userPasswordChangeRequest);
-    void resetPassword(UserPasswordResetRequest userPasswordResetRequest);
-
 }

6c3e9903675f6898ff631d150733bf4a4b7b2436
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-13 22:05:44 +0100

    contact_management_service

--- src/main/java/africa/semicolon/contactManagementService/dtos/requests/UserLoginRequest.java
+++ src/main/java/africa/semicolon/contactManagementService/dtos/requests/UserLoginRequest.java
@@ -1,4 +1,11 @@
 package africa.semicolon.contactManagementService.dtos.requests;
 
+import lombok.Data;
+
+@Data
 public class UserLoginRequest {
+
+    private int userId;
+    private String email;
+
 }

bb10a3756de26f2ac8674db6caedee5fc0f36b52
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-13 22:11:00 +0100

    contact_management_service

--- src/main/java/africa/semicolon/contactManagementService/utility/Mapper.java
+++ src/main/java/africa/semicolon/contactManagementService/utility/Mapper.java
@@ -2,8 +2,10 @@ package africa.semicolon.contactManagementService.utility;
 
 import africa.semicolon.contactManagementService.datas.models.Contact;
 import africa.semicolon.contactManagementService.datas.models.User;
-import africa.semicolon.contactManagementService.dtos.ContactCreationRequest;
-import africa.semicolon.contactManagementService.dtos.UserCreationRequest;
+import africa.semicolon.contactManagementService.dtos.requests.ContactCreationRequest;
+import africa.semicolon.contactManagementService.dtos.requests.UserCreationRequest;
+import africa.semicolon.contactManagementService.dtos.responses.ContactCreationResponse;
+import africa.semicolon.contactManagementService.dtos.responses.UserRegistrationResponse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,13 +17,12 @@ public class Mapper {
     public static User map(UserCreationRequest userCreationRequest) {
         User newUser = new User();
         newUser.setUsername(toLowerCase(userCreationRequest.getUsername()));
-        newUser.setPassword(userCreationRequest.getPassword());
         newUser.setEmail(userCreationRequest.getEmail());
         return newUser;
     }
 
-    public static boolean checkEmptyStringUsername(String username) {
-        return username.isEmpty();
+    public static boolean isEmptyOrNullString(String title) {
+        return title == null || title.isEmpty();
     }
 
     public static List<?> checkIfListIsNull(List<?> lists) {
@@ -36,5 +37,22 @@ public class Mapper {
         newContact.setName(STR."\{contactCreationRequest.getFirstName()} \{contactCreationRequest.getLastName()}");
         newContact.setPhoneNumber(contactCreationRequest.getPhoneNumber());
         return newContact;
+    }
+
+    public static UserRegistrationResponse map(User user){
+        UserRegistrationResponse userRegistrationResponse = new UserRegistrationResponse();
+        userRegistrationResponse.setUserId(user.getId());
+        userRegistrationResponse.setUsername(user.getUsername());
+        userRegistrationResponse.setEmail(user.getEmail());
+        return userRegistrationResponse;
+    }
+
+    public static ContactCreationResponse map(User user, Contact contact) {
+        ContactCreationResponse contactCreationResponse = new ContactCreationResponse();
+        contactCreationResponse.setUserId(user.getId());
+        contactCreationResponse.setUsername(user.getUsername());
+        contactCreationResponse.setContactName(contact.getName());
+        contactCreationResponse.setContactId(contact.getId());
+        return contactCreationResponse;
     }
 }

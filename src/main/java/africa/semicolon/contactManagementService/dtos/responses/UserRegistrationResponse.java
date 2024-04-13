72138234cd3eea98449028903593a62a2b99c7e2
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-13 22:09:25 +0100

    contact_management_service

--- src/main/java/africa/semicolon/contactManagementService/dtos/responses/UserRegistrationResponse.java
+++ src/main/java/africa/semicolon/contactManagementService/dtos/responses/UserRegistrationResponse.java
@@ -1,4 +1,11 @@
 package africa.semicolon.contactManagementService.dtos.responses;
 
+import lombok.Data;
+
+@Data
 public class UserRegistrationResponse {
+
+    private int userId;
+    private String username;
+    private String email;
 }

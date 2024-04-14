7f6c567d421c3b0d97de61f43a3afce719f356dd
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-14 17:42:59 +0100

    contact_management_service

--- src/main/java/africa/semicolon/contactManagementService/services/userService/UserService.java
+++ src/main/java/africa/semicolon/contactManagementService/services/userService/UserService.java
@@ -1,12 +1,14 @@
 package africa.semicolon.contactManagementService.services.userService;
 
+import africa.semicolon.contactManagementService.datas.models.Contact;
+import africa.semicolon.contactManagementService.datas.models.Group;
 import africa.semicolon.contactManagementService.datas.models.User;
-import africa.semicolon.contactManagementService.dtos.*;
-import africa.semicolon.contactManagementService.dtos.requests.ContactCreationRequest;
-import africa.semicolon.contactManagementService.dtos.requests.UserCreationRequest;
-import africa.semicolon.contactManagementService.dtos.requests.UserLoginRequest;
+import africa.semicolon.contactManagementService.dtos.requests.*;
 import africa.semicolon.contactManagementService.dtos.responses.ContactCreationResponse;
+import africa.semicolon.contactManagementService.dtos.responses.GroupCreationResponse;
 import africa.semicolon.contactManagementService.dtos.responses.UserRegistrationResponse;
+import africa.semicolon.contactManagementService.dtos.responses.UserUpdateResponse;
+import africa.semicolon.contactManagementService.exception.ContactUpdateResponse;
 
 import java.util.List;
 
@@ -20,4 +22,24 @@ public interface UserService {
     User searchUserById(int userId);
 
     void loginUser(UserLoginRequest userLoginRequest);
+
+    void logoutUser(int id);
+
+    UserUpdateResponse updateUser(UserUpdateRequest userUpdateRequest);
+
+    void deleteUser(int id);
+
+    ContactUpdateResponse updateContact(ContactUpdateRequest contactUpdateRequest);
+
+    GroupCreationResponse createGroup(GroupCreationRequest groupCreationRequest);
+
+    void updateGroup(UpdateGroupRequest updateGroupRequest);
+
+    void deleteGroup(GroupDeleteRequest groupDeleteRequest);
+
+    void deleteContact(ContactDeleteRequest contactDeleteRequest);
+
+    List<Contact> findAllContact(int userId);
+
+    List<Group> findAllGroup(int userId);
 }

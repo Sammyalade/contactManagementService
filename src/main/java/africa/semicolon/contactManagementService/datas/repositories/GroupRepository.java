dcdb284dc921484a6566fba9c4aa9bbee9304e82
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-14 17:36:42 +0100

    contact_management_service

--- src/main/java/africa/semicolon/contactManagementService/datas/repositories/GroupRepository.java
+++ src/main/java/africa/semicolon/contactManagementService/datas/repositories/GroupRepository.java
@@ -4,4 +4,5 @@ import africa.semicolon.contactManagementService.datas.models.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupRepository extends JpaRepository<Group, Integer> {
+    Group findByName(String name);
 }
